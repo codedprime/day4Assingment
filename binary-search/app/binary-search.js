c6e9558e079552f8bd6098c95fa0161e75261c01
Obinna Okwuolisa <codedprime@users.noreply.github.com>
2017-03-18 09:51:17 +0100

    Update binary-search.js

--- binary-search/app/binary-search.js
+++ binary-search/app/binary-search.js
@@ -44,16 +44,16 @@ Array.prototype.toOneThousand = function() {
 
         }
         
- return myArray;
+        return myArray;
   
 };
 Array.prototype.search = function (input) {
-      var length  = this.length;
-      var    max    = length;
-      var   min     = 0;
-      var   mid     = min + Math.floor((max - min)/2);
-      var  count   = 0;
-        
+        var length  = this.length;
+        var    max    = length;
+        var   min     = 0;
+        var   mid     = min + Math.floor((max - min)/2);
+        var  count   = 0;
+
         while (this[mid - 1] !== input) {
             if (input > this[mid - 1]){
                 min  = mid + 1;
@@ -63,25 +63,25 @@ Array.prototype.search = function (input) {
             }
             mid  = min + Math.ceil((max - min)/2);
 
-            
+
             if (min === mid && this[mid - 1] !== input){
                 return new keep (count, length, -1);
             } 
         }
-       
+
         if (input === this[length - 1]) return {
             length: length, index: (max - 1), count: 0
         };
-        
+
         return new keep (count, length, (mid - 1));
 };
 
 
 
 function keep(count,length,index){
-    this.count = count;
-    this.length = length;
-    this.index = index;
+        this.count = count;
+        this.length = length;
+        this.index = index;
   
 }
 module.exports = Array.prototype;
